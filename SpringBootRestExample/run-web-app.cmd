09d00ec8a50316194ba45fbc1a2398c2fecc4f1b
ABN <abn.java@gmail.com>
2018-08-26 23:43:50 +0300

    clearing

--- SpringBootRestExample/run-web-app.cmd
+++
@@ -1,6 +0,0 @@
-
-cd /d %~dp0
-
-CALL mvn spring-boot:run -Drun.jvmArguments="-Drunning.from.maven=true"
-
-PAUSE
--- SpringBootRestExample/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into user values( 1001, '1971-01-01', 'JOHN1', 'DOE1' );
-insert into user values( 1002, '1972-02-02', 'JOHN2', 'DOE2' );
-insert into user values( 1003, '1973-03-03', 'JOHN3', 'DOE3' );
-insert into user values( 1004, '1974-04-04', 'JOHN4', 'DOE4' );
-insert into user values( 1005, '1975-05-05', 'JOHN5', 'DOE5' );
